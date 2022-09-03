394c472804012269446e1da10a5009ddd582a199
Kate Belozertseva <katebelozertseva@gmail.com>
2022-09-03 19:05:36 +0300

    Задания к первому семинару!

---
+++ ВсеЧетныеОт1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите другое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+while (count < num)
+{
+     if(num % 2 == 0)
+     {
+     count = (count + 2);
+     }
+     
+     else
+     {
+     count = (count + 2);
+     num = (num-1);
+     }
+ Console.Write($"{count}, ");
+}
+
---
+++ МаксИзТрех/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+
+Console.Clear();
+
+Console.WriteLine("Enter the number: "); 
+int numA  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: "); 
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number: "); 
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if(numB > max)
+{
+    max = numB;
+}
+if(numC > max)
+{
+    max = numC;
+}
+else
+{
+    max = numA;
+}
+Console.WriteLine($"Максимальное число: {max}");
